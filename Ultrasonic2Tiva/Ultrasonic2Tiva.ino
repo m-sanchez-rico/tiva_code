529020e742c55aa25456392e75197901babfaf1b
Miguel I. Sanchez <migueli.sanchez.rico@gmail.com>
2019-07-29 17:24:32 +0200

    msan: Ultrasonic sensor working

--- Ultrasonic2Tiva/Ultrasonic2Tiva.ino
+++ Ultrasonic2Tiva/Ultrasonic2Tiva.ino
@@ -1,8 +1,8 @@
 
 //global variables
-const int echo=9, Trig=10;
-long duration, cm;
-
+#define echo 9
+#define Trig 10
+#define LED GREEN_LED
 
 
 void setup() 
@@ -12,6 +12,7 @@ void setup()
 
   Serial.begin (115200);
   SetupUltrasonic(); //function
+
 }
 
 void loop() 
@@ -26,32 +27,47 @@ void loop()
 //Definition of function
 void SetupUltrasonic()
 {
-  pinMode (Trig, OUTPUT );
-  pinMode (echo, INPUT  );
+  pinMode(Trig, OUTPUT);
+  pinMode(echo, INPUT );
+  pinMode(LED , OUTPUT); 
 }
 
 void Update_Ultra_Sonic()
 {
+  
   digitalWrite (Trig, LOW);
+  digitalWrite (LED, LOW);
   delayMicroseconds(2);
   digitalWrite (Trig, HIGH);
+  digitalWrite (LED, HIGH);
   delayMicroseconds(10);
   digitalWrite (Trig, LOW);
+  digitalWrite (LED, LOW);
 
+  long duration;
   duration = pulseIn(echo, HIGH);
-
+  Serial.print("Reading duration from pulseIn(echo, HIGH)= ");
+  Serial.print(duration);
+  Serial.print("\n");
+  
   //convert time to distance
+  long cm;
   cm=microsecndsToCentimeters(duration);
 
   //Sending through serial port
-  Serial.print("distance=");
-  Serial.print("\t");
+  Serial.print("-distance= ");
   Serial.print(cm);
+  Serial.print("cm -duration= ");
+  Serial.print(duration);
+  Serial.print("us");
   Serial.print("\n");
 }
 
 long microsecndsToCentimeters(long microseconds)
 {
   //340 m/s = 0.034 cm/us = 1/0.034 us/cm = 29.41 us/cm 
-  return microseconds /29 /2;
+  long ret;
+  //ret= (microseconds/2)/58;
+  ret = microseconds*0.017;
+  return ret;
 }
