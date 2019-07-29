478a4bc4f9a654d632d1a4e0895bdbc62c69aa4b
Miguel I. Sanchez <migueli.sanchez.rico@gmail.com>
2019-07-29 17:39:47 +0200

    msan: tested and working

--- Ultrasonic2Tiva/Ultrasonic2Tiva.ino
+++ Ultrasonic2Tiva/Ultrasonic2Tiva.ino
@@ -34,40 +34,41 @@ void SetupUltrasonic()
 
 void Update_Ultra_Sonic()
 {
+  long duration, cm;
   
   digitalWrite (Trig, LOW);
   digitalWrite (LED, LOW);
   delayMicroseconds(2);
+  
   digitalWrite (Trig, HIGH);
   digitalWrite (LED, HIGH);
   delayMicroseconds(10);
+  
   digitalWrite (Trig, LOW);
   digitalWrite (LED, LOW);
 
-  long duration;
+  
   duration = pulseIn(echo, HIGH);
-  Serial.print("Reading duration from pulseIn(echo, HIGH)= ");
-  Serial.print(duration);
-  Serial.print("\n");
   
   //convert time to distance
-  long cm;
   cm=microsecndsToCentimeters(duration);
 
   //Sending through serial port
-  Serial.print("-distance= ");
-  Serial.print(cm);
-  Serial.print("cm -duration= ");
+  Serial.print("-duration= ");
   Serial.print(duration);
-  Serial.print("us");
+  Serial.print("us -distance= ");
+  Serial.print(cm);
+  Serial.print("cm");
   Serial.print("\n");
 }
 
 long microsecndsToCentimeters(long microseconds)
 {
-  //340 m/s = 0.034 cm/us = 1/0.034 us/cm = 29.41 us/cm 
   long ret;
+  //340 m/s = 0.034 cm/us = 1/0.034 us/cm = 29.41 us/cm
   //ret= (microseconds/2)/58;
+  
   ret = microseconds*0.017;
+  
   return ret;
 }
